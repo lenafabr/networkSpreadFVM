a95ed720ead237115f6cb79e776e9c66740c0f8e
Lena <lenafabr@gmail.com>
2021-03-25 14:34:27 -0700

    initial commit, basic matlab read-in scripts

---
+++ scripts/MeshObj.m
@@ -0,0 +1,108 @@
+classdef MeshObj < handle
+    % object defining a FVM mesh
+properties                               
+    ncell % number of mesh cells
+    dim % dimensionality of network
+    bounds % list of neighbor cells bordering each mesh cell 
+    len % total length of tubule associated with this mesh cell
+    pos % position of the center of each cell
+    deg % degree of each cell (number of neighbors)
+    maxdeg % maximal degree of all cells in the mesh
+    resvind % reservoir index
+    nodeind % which node index
+    % 1st column: which network edge, 2nd col: which mesh cell index along that edge
+    edgeind 
+end
+
+methods
+    function MSH = MeshObj(fname)
+        % create a network object
+        % optionally, load data from file
+        
+        MSH.ncell = 0; % number of nodes        
+        MSH.dim = 2; % spatial dimension        
+        MSH.maxdeg = 10; % maximum allowed degree              
+        
+        if (nargin>0)
+            MSH.loadMesh(fname);
+        end
+    end        
+    
+    function loadMesh(MSH, filename)
+        % load in mesh object from file        
+        data = dlmread(filename);
+        ncell = data(1,1);
+        dim = data(1,2);
+        
+        MSH.ncell = ncell;
+        MSH.dim = dim;
+        MSH.pos = data(2:1+ncell,2:dim+1);
+        MSH.len = data(2:1+ncell,dim+2);
+        MSH.deg = data(2:ncell+1,dim+3);
+        MSH.maxdeg = max(MSH.deg);
+        
+        % Load in connectivity info and lengths
+        MSH.bounds = zeros(ncell,MSH.maxdeg);   
+        MSH.edgeind = zeros(ncell,2);
+        for cc = 1:MSH.ncell
+            deg = MSH.deg(cc);
+            MSH.bounds(cc,1:deg) = data(cc+1,dim+4:dim+3+deg);  
+            if (size(data,2)>dim+deg+3)
+                if (size(data,2)>dim+deg+4) % nodeind and edgeind included
+                    MSH.nodeind(cc) = data(cc+1,dim+deg+4);
+                    MSH.edgeind(cc,:) = data(cc+1,dim+deg+(5:6));
+                    MSH.resvind(cc) = data(cc+1,dim+deg+7);
+                else
+                    % no node/edge info
+                    MSH.nodeind(cc) = 0; MSH.edgeind(cc,:) = 0;
+                    MSH.resvind(cc) = data(cc+1,dim+deg+4);
+                end
+            else
+                MSH.nodeind(cc) = 0; MSH.edgeind(cc,:) = 0;
+                MSH.resvind(cc) = 0; % no reservoir info
+            end
+        end
+    end
+    
+    function plotField(MSH,field,climits)
+        % plot field value on the mesh as colored lines
+        
+        colormap(copper)
+        caxis(climits)
+        cmap = copper;
+        
+        allX1 = []; allX2 = []; allY1 = []; allY2 = []; allcvals=[];
+        for bct = 1:MSH.maxdeg
+            
+            % direction to the bct-th boundary for each node
+            blist = MSH.bounds(:,bct);
+            ind = find(blist>0);
+            bdir = MSH.pos(blist(ind),:)-MSH.pos(ind,:);
+            bdir = bdir./sqrt(sum(bdir.^2,2));
+            %
+            X1 = MSH.pos(ind,1);
+            X2 = MSH.pos(ind,1)+MSH.len(ind)/2.*bdir(:,1);
+            Y1 = MSH.pos(ind,2);
+            Y2 = MSH.pos(ind,2)+MSH.len(ind)/2.*bdir(:,2);
+            
+            allX1 = [allX1; X1];
+            allX2 = [allX2; X2];
+            allY1 = [allY1; Y1];
+            allY2 = [allY2; Y2];
+            
+            
+            clamped = field(ind);
+            clamped(clamped<climits(1)) = climits(1);
+            clamped(clamped>climits(2)) = climits(2);
+            cvals = interp1(linspace(climits(1),climits(2),size(cmap,1)), cmap, clamped);
+            allcvals = [allcvals; cvals];
+        end
+           
+        set(gca,'ColorOrder',allcvals,'NextPlot', 'replacechildren');
+           
+        plot([allX1 allX2]', [allY1 allY2]','LineWidth',3)
+            
+        axis equal
+    end    
+end
+end
---
+++ scripts/loadSnapshotFVM.m
@@ -0,0 +1,65 @@
+
+function [fields,snaptimes,vels,CL ] = loadSnapshotFVM(filename)
+    %% load FVM snapshot data
+    % also returns a "cell object" structure that can be used for
+    % processing
+    % later as if it was experimental data
+
+   data = dlmread(filename);
+   nfield = data(1,1);
+   ncell = data(1,2);
+   maxdeg = data(1,3);
+   
+   % are velocities included in these snapshots as well?
+   readvel = data(1,5);
+   
+   % number of lines in each snapshot
+   if (readvel>0) 
+        snaplines = nfield+2;
+   else
+       snaplines = nfield+1;
+   end
+    
+   nsnap = size(data,1)/snaplines;
+   
+   snaptimes = data(1:snaplines:end,4);   
+   if (readvel>0)
+        vels = data(2:snaplines:end,:);
+   else
+       vels = NaN;
+   end
+   
+   %%
+   fields = zeros(nsnap,ncell,nfield);
+   if (readvel>0) 
+       start = 2;
+   else
+       start = 1;
+   end
+   for fc = 1:nfield
+        fields(:,:,fc) = data(start+fc:snaplines:end,1:ncell);
+   end
+   
+   % reorder dimensions as: cell, field, time
+   fields = permute(fields,[2,3,1]);
+   
+   if (readvel>0)
+       % reorganize velocities into dimensions: cell, boundary, time
+       vels = reshape(vels,[nsnap,ncell,maxdeg]);
+       vels = permute(vels,[2,3,1]);
+   end
+   
+   % create a "Cell object" structure containing some minimal fields
+   % to be used in analyzing snapshots as if they were experimental data
+   if (nargout>3)
+       if (~exist('CellObjPA','class'))
+           warning('Cell object class not defined.')
+           CL = NaN;
+       else
+           CL = CellObjPA(filename);
+           CL.NFrame = nsnap;
+           CL.dt = snaptimes(2)-snaptimes(1);
+           CL.startPA = 1;
+       end
+   end
+end
