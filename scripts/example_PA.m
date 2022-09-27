e24936d1bcc251f6ec40c57bd21a81722530e691
Elena Koslover <ekoslover@ucsd.edu>
2022-09-27 13:52:53 -0700

    added PA example

--- scripts/example_PA.m
+++ scripts/example_PA.m
@@ -1,8 +1,8 @@
 % example code to load and visualize results
 % of bolus spreading from FVM simulations
 
-%% load network
-%load('~/proj/propagatormethods/networks/WT_COS7_KDEL_Cell3_um.mat','NT')
+% load network object for visualizing (optional)
+%NT = NetworkObj('../examples/WT_COS7_KDEL_Cell3_um.net');
 
 %% Load in mesh data
 dirname = '../examples/';
@@ -21,14 +21,15 @@ cmap = copper(200);
 clear M
 
 ct = 0;
+figure
+colormap(cmap)    
 for sc = 1%:nsnap
     ct = ct+1;
     sc
        
     scatter(MSH.pos(:,1),MSH.pos(:,2),20,field(:,1,sc),'filled')    
     
-    caxis([0,0.1])
-    colormap(cmap)    
+    caxis([0,0.1])   
         
     set(gca,'Visible','off','Position',[0 0 1 1],'YDir','reverse')
     axis equal
